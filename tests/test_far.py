fe952df5feacfb8c07b7268506a09e1016d13670
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-06-15 18:42:10 +0530

    Add test case when old/new is None

--- tests/test_far.py
+++ tests/test_far.py
@@ -19,16 +19,25 @@ class TestFar(unittest.TestCase):
     def setUp(self):
         self.old = 'asdfadfas'
         self.new = 'asdfadfas'
+        self.obj = Far()
 
-    def tearDown(self):
-        pass
+    @patch('os.system')
+    def test_should_do_nothing_if_old_is_none(self, mock_system):
+        mock_system.return_value = 0
+        self.obj.find_and_replace(None, self.new)
+        self.assertEqual(mock_system.called, 0)
+
+    @patch('os.system')
+    def test_should_do_nothing_if_new_is_none(self, mock_system):
+        mock_system.return_value = 0
+        self.obj.find_and_replace(self.old, None)
+        self.assertEqual(mock_system.called, 0)
 
     @patch('os.system')
     def test_should_run_system_command(self, mock_system):
         mock_system.return_value = 0
 
-        obj = Far()
-        obj.find_and_replace(self.old, self.new)
+        self.obj.find_and_replace(self.old, self.new)
 
         cmd = "find . -type f -not -path '*/\.git*' -exec sed -i 's/" + self.old + "/" + self.new + "/g' {} +  "
         mock_system.assert_called_once_with(cmd)

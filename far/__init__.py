043215f8aa321450ea4b94a6406289cf9f637dfa
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-06-15 18:13:43 +0530

    Update setup script

--- far/__init__.py
+++ far/__init__.py
@@ -1,1 +1,3 @@
 from .far import Far
+
+__version__ = '0.0.1'
--- setup.py
+++ setup.py
@@ -1,30 +1,40 @@
 from distutils.core import setup
+from setuptools import find_packages
+
+def get_version():
+    import far
+    return far.__version__
+
+def get_requirements():
+    with open('requirements.txt', 'rU') as fhan:
+        requires = [line.strip() for line in fhan.readlines()]
+    return requires
+
+def get_long_description():
+    try:
+        import pypandoc
+        long_description = pypandoc.convert('README.md', 'rst')
+    except (IOError, ImportError):
+        with open('README.txt') as fhan:
+            long_description = fhan.read()
+    return long_description
+
 
 add_keywords = dict(
     entry_points={
         'console_scripts': ['far = far.main:main'],
     }, )
 
-fhan = open('requirements.txt', 'rU')
-requires = [line.strip() for line in fhan.readlines()]
-fhan.close()
-#print('We require: ', requires)
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError):
-    fhan = open('README.txt')
-    long_description = fhan.read()
-    fhan.close()
-
 setup(
     name='Far',
     description='Find and Replace',
-    version='0.0.1',
-    packages=['far'],
+    version=get_version(),
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*",
+                                    "tests"]),
     license='GPLv3+',
     author='Shubham Chaudhary',
     author_email='me@shubhamchaudhary.in',
     url='https://github.com/shubhamchaudhary/far',
-    long_description=long_description,
-    install_requires=requires, **add_keywords)
+    long_description=get_long_description(),
+    install_requires=get_requirements(),
+    **add_keywords)
